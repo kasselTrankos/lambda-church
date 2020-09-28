ac4df2429e7e87e03ab5c0aa6093e0ddf7aeb3e3
xe71086 <alvaro.touzon.contractor@bbva.com>
2020-09-28 20:08:41 +0200

    feat: udate lamda cond

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // ZERO := ƛfx.x 
 const ZERO = f => x => x;
+
 // T = K := ƛxy.x
 const K = x => y => x;
 // T := ƛxy.x
@@ -16,12 +17,18 @@ const SUCC = num => f => x => f(num(f)(x));
 
 // ADD := ƛnk.n(SUCC)k
 const ADD = n => k => n(SUCC)(k);
+// B := λab.a∘b = compose
+const B = a => b => x =>a(b(x));
+
+// MULT := λab.a∘b = compose
+const MULT = B;
 
 // NOT := ƛb.bFT
 const NOT = b => b(F)(T)
 
 // AND := ƛpq.pTq
-const AND = p => q => p(q)(F)
+const AND = p => q => p(q)(F);
+
 
 // OR := ƛpq.pT.q
 const OR = p => q => p(T)(q);
@@ -31,46 +38,71 @@ const I = x => x;
 const obj = {a: 1, b: 2};
 const names = obj => Object.getOwnPropertyNames(obj);
 
+// is0 : = ƛn.n(KF)T
+const is0 = n => n(K(F))(T);
+
 
 const SUM = x => x + 1;
 const n0 = ZERO;
+
 const n1 = SUCC(n0);
 const n2 = SUCC(n1);
 const n3 = SUCC(n2);
 const n4 = SUCC(n3);
 
 
-const jsnum = n => n(x => x + 1)(0)
+const jsnum = n => n(x => x + 1)(0);
+const jstag = n => n(x =>  `<spam>${x}</spam>`)('hoila');
 
 const TAG = name => value => `<${name}>${value}</${name}>`;
 
 const SPAM = TAG('spam');
 const KEYS = ONCE(names)(obj);
 
-console.log('1: ', n0(SUM)(0) );
-console.log('ONCE 1: ', n1(SUM)(0) );
 
-console.log('hola spam: ', n0(SPAM)('hola mundo'));
-console.log('ONCE hola spam: ', n1(SPAM)('hola mundo'));
+// console.log('im zero ', jsnum(n0));
+// console.log('im zero ', jsnum(n0));
+// console.log('im zero 2 ', jsnum(n2));
+// console.log('im zero 3 ', jsnum(n4));
 
-console.log('K 1: ', K(1)(0));
-console.log('K 11: ', K(11)(10));
-console.log('K I:', K(I)(11)(10));
-console.log(ONCE(names)(obj));
-const _or = OR(F)(T);
-const _and = AND(F)(T);
+// console.log(' SUMO n3 + n4 = n7', jsnum(ADD(n3)(n4)));
+// console.log(' SUMO n3 x n4 = n12', jsnum(MULT(n3)(n4)));
 
-const _not = NOT(_or);
-const _not_and = NOT(_and);
+console.log(jstag(ADD(n3)(n1)));
+console.log(is0(n1))
 
-console.log('OR: ', _or(9)(19));
-console.log('AND: ', _and(9)(19));
-console.log('OR NOT: ', _not(9)(19));
-console.log('AND NOT: ', _not_and(9)(19));
-console.log(SUCC(SUCC(ONCE))(NOT)(T))
-console.log(jsnum(n0), '1 0000')
-console.log(jsnum(n1), '2 0000')
-console.log(jsnum(ADD(n3)(n1)))
+
+// var t = 0 ? 'd':'f';
+// console.log(T(1)(3));
+// console.log(F(1)(3));
+
+
+// console.log('ADD 0 + 2 ', jsnum(ADD(n2)(n1)) );
+// console.log('MULT 3 x 3 ', jsnum(MULT(n3)(n3)) );
+
+// console.log('ONCE 1: ', SUCC(jsnum(n1)));
+
+// console.log('hola spam: ', n0(SPAM)('hola mundo'));
+// console.log('ONCE hola spam: ', n1(SPAM)('hola mundo'));
+
+// console.log('K 1: ', K(1)(0));
+// console.log('K 11: ', K(11)(10));
+// console.log('K I:', K(I)(11)(10));
+// console.log(ONCE(names)(obj));
+// const _or = OR(F)(T);
+// const _and = AND(F)(T);
+
+// const _not = NOT(_or);
+// const _not_and = NOT(_and);
+
+// console.log('OR: ', _or(9)(19));
+// console.log('AND: ', _and(9)(19));
+// console.log('OR NOT: ', _not(9)(19));
+// console.log('AND NOT: ', _not_and(9)(19));
+// console.log(SUCC(SUCC(ONCE))(NOT)(T))
+// console.log(jsnum(n0), '1 0000')
+// console.log(jsnum(n1), '2 0000')
+// console.log(jsnum(ADD(n3)(n1)))
 
 
 
